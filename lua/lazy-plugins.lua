a2241839be08f331905971c5298bf7d22e24ce4e
Romeao Jennings <romeao@amazon.com>
2024-11-29 09:27:06 -0800

    Initial configuration

---
+++ lua/lazy-plugins.lua
@@ -0,0 +1,18 @@
+require('lazy').setup {
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  require 'plugins/gitsigns',
+  require 'plugins/which-key',
+  require 'plugins/telescope',
+  require 'plugins/lspconfig',
+  require 'plugins/conform',
+  require 'plugins/cmp',
+  require 'plugins/todo-comments',
+  require 'plugins/mini',
+  require 'plugins/treesitter',
+  require 'plugins/debug',
+  require 'plugins/indent_line',
+  require 'plugins/autopairs',
+  require 'plugins/neo-tree',
+  require 'plugins/catppuccin'
+}
